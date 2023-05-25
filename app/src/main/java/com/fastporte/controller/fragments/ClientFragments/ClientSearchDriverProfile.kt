910adbc8416a724bcfab37ec59d38017575c778f
diegoTS18 <diegotasa35@hotmail.com>
2023-05-25 11:28:54 -0500

    feat(searchvehicle): show driver profile information general

--- app/src/main/java/com/fastporte/controller/fragments/ClientFragments/ClientSearchDriverProfile.kt
+++ app/src/main/java/com/fastporte/controller/fragments/ClientFragments/ClientSearchDriverProfile.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.fastporte.R
+import com.fastporte.models.User
 import com.google.android.material.tabs.TabLayout
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ClientSearchDriverProfile : Fragment() {
-
+    lateinit var user: User;
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager: ViewPager
 
@@ -22,6 +30,9 @@ class ClientSearchDriverProfile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view: View= inflater.inflate(R.layout.fragment_client_search_driver_profile, container, false)
+
+        loadInformation(view)
+        contractDriver(view)
 
         tabLayout = view.findViewById(R.id.tab_layout)
         viewPager = view.findViewById(R.id.view_pager)
@@ -36,6 +47,28 @@ class ClientSearchDriverProfile : Fragment() {
         tabLayout.setupWithViewPager(viewPager)
 
         return view
+    }
+
+    private fun contractDriver(view_: View) {
+        val btSearchVehicleContract = view_.findViewById<Button>(R.id.btSearchVehicleContract)
+        btSearchVehicleContract.setOnClickListener(){
+
+        }
+    }
+
+    private fun loadInformation(view_: View) {
+        val tvSearchVehicleProfileName = view_.findViewById<TextView>(R.id.tvSearchVehicleProfileName)
+        val tvSearchVehicleProfileDescription = view_.findViewById<TextView>(R.id.tvSearchVehicleProfileDescription)
+        val rbSearchVehicleUserStar = view_.findViewById<RatingBar>(R.id.rbSearchVehicleUserStar)
+        val civtvSearchVehicleCarrierProfile = view_.findViewById<CircleImageView>(R.id.civtvSearchVehicleCarrierProfile)
+        user = arguments?.getSerializable("searchUserTemp") as User
+        tvSearchVehicleProfileName.text = user.name
+        tvSearchVehicleProfileDescription.text = user.description
+
+        val picBuilder= Picasso.Builder(requireContext())
+        picBuilder.downloader((OkHttp3Downloader(context)))
+        picBuilder.build().load(user.photo).placeholder(R.color.white).error(R.color.white).into(civtvSearchVehicleCarrierProfile)
+
     }
 
     private inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
--- app/src/main/java/com/fastporte/controller/fragments/ClientFragments/SearchResultFragment.kt
+++ app/src/main/java/com/fastporte/controller/fragments/ClientFragments/SearchResultFragment.kt
@@ -7,11 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fastporte.R
+import com.fastporte.adapter.SearchClientCommentsAdapter
+import com.fastporte.models.CommentsSearch
+import com.fastporte.models.User
+import com.fastporte.network.CommentsService
+import com.fastporte.network.DriversService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchResultFragment : Fragment() {
-
+    lateinit var user: User;
 
 
     override fun onCreateView(
@@ -37,8 +49,33 @@ class SearchResultFragment : Fragment() {
     private fun simulate(view_: View) {
         val btnNext = view_.findViewById<Button>(R.id.simulate)
         btnNext.setOnClickListener() {
-            Navigation.findNavController(view_)
-                .navigate(R.id.action_searchResultFragment_to_clientSearchDriverProfile)
+
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api-fastporte.azurewebsites.net/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val driverService: DriversService = retrofit.create(DriversService::class.java)
+            val driverById = driverService.getDriver(1,"json")
+            driverById.enqueue(object :Callback<User>{
+                override fun onResponse(call: Call<User>, response: Response<User>) {
+                    if(response.isSuccessful){
+                        user=response.body()!!
+                        val bundle = Bundle()
+                        bundle.putSerializable("searchUserTemp", user)
+                        Navigation.findNavController(view_)
+                            .navigate(R.id.action_searchResultFragment_to_clientSearchDriverProfile,bundle)
+                    }
+                }
+
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
+
+
 
         }
     }
