25713988c88593f79eeb60bdd063e872a99cd686
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2023-06-13 04:39:01 -0500

    feat(notifications): contract payment from notifications

--- app/src/main/java/com/fastporte/controller/fragments/ClientFragments/ClientPayContractFragment.kt
+++ app/src/main/java/com/fastporte/controller/fragments/ClientFragments/ClientPayContractFragment.kt
@@ -1,19 +1,27 @@
 package com.fastporte.controller.fragments.ClientFragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.fastporte.R
+import com.fastporte.controller.activities.MainActivity
 import com.fastporte.helpers.BaseURL
 import com.fastporte.helpers.SharedPreferences
+import com.fastporte.models.CardClient
 import com.fastporte.models.ClientNotification
-import com.fastporte.models.User
+import com.fastporte.network.CardService
 import com.fastporte.network.NotificationService
-import com.fastporte.network.ProfileService
+import com.google.android.material.textfield.TextInputLayout
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import retrofit2.Call
@@ -35,6 +43,8 @@ class ClientPayContractFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_client_pay_contract, container, false)
 
+
+        paymentFormConstraints(view)
         loadData(view)
         return view
     }
@@ -54,8 +64,6 @@ class ClientPayContractFragment : Fragment() {
         request.enqueue(object : Callback<ClientNotification> {
             override fun onResponse(call: Call<ClientNotification>, response: Response<ClientNotification>) {
                 if (response.isSuccessful) {
-                    println("RESPONSEEEEEEEEEEE")
-                    println("RESPONSE: "+response.body()!!)
                     showData(response.body()!!)
                 }
                 else    {
@@ -64,7 +72,6 @@ class ClientPayContractFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<ClientNotification>, t: Throwable) {
-                println("FALLOOOOOOO")
                 println("Error: ${t.message}")
             }
         })
@@ -107,4 +114,210 @@ class ClientPayContractFragment : Fragment() {
 
     }
 
+    private fun paymentFormConstraints(view: View){
+        val tilYear = view.findViewById<TextInputLayout>(R.id.til_year)
+        val tilMonth = view.findViewById<TextInputLayout>(R.id.til_month)
+        val tilCvv = view.findViewById<TextInputLayout>(R.id.til_cvv)
+        val tilHolderName = view.findViewById<TextInputLayout>(R.id.til_holder)
+        val tilCardNumber = view.findViewById<TextInputLayout>(R.id.til_number)
+
+        val tilNumberLength = 16
+        val tilYearLength = 4
+        val tilMonthLength = 2
+        val tilCvvLength = 3
+
+        tilYear.error = " "
+        tilMonth.error = " "
+        tilCvv.error = " "
+        tilHolderName.error = " "
+        tilCardNumber.error = " "
+
+        var _holderName: String? = null
+        var _cardNumber: Long? = null
+        var _month: String? = null
+        var _year: String? = null
+        var _cvv: Int? = null
+
+        val btPay = view.findViewById<Button>(R.id.bt_pay)
+        btPay.isEnabled = false
+        fun updateButtonState() {
+            btPay.isEnabled = tilYear.error == null &&
+                    tilMonth.error == null &&
+                    tilCvv.error == null &&
+                    tilHolderName.error == null &&
+                    tilCardNumber.error == null
+        }
+
+        tilHolderName.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length == 0) {
+                    tilHolderName.error = "Invalid holder name"
+                    tilHolderName.isErrorEnabled = true
+                } else {
+                    tilHolderName.error = null
+                    tilHolderName.isErrorEnabled = false
+                }
+                updateButtonState()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){}
+        })
+
+        tilCardNumber.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length == 0 || s?.length != tilNumberLength) {
+                    tilCardNumber.error = "Card number must have $tilNumberLength digits"
+                    tilCardNumber.isErrorEnabled = true
+                } else {
+                    tilCardNumber.error = null
+                    tilCardNumber.isErrorEnabled = false
+                }
+                updateButtonState()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){}
+
+        })
+
+        val tilYearEditText = tilYear.editText
+        tilYearEditText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length != tilYearLength) {
+                    tilYear.error = "Invalid year"
+                    tilYear.isErrorEnabled = true
+                } else {
+                    tilYear.error = null
+                    tilYear.isErrorEnabled = false
+                }
+
+                if ( (s?.length == tilYearLength) && (s.toString().toInt() < 2023)) {
+                    tilYear.error = "Invalid year"
+                    tilYear.isErrorEnabled = true
+                } else if ( (s?.length == tilYearLength) && (s.toString().toInt() >= 2023)) {
+                    tilYear.error = null
+                    tilYear.isErrorEnabled = false
+                }
+
+                updateButtonState()
+
+            }
+        })
+
+        val tilCvvEditText = tilCvv.editText
+        tilCvvEditText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length != tilCvvLength) {
+                    tilCvv.error = "Invalid CVV"
+                    tilCvv.isErrorEnabled = true
+                } else {
+                    tilCvv.error = null
+                    tilCvv.isErrorEnabled = false
+                }
+                updateButtonState()
+            }
+        })
+
+        val tilMonthEditText = tilMonth.editText
+        tilMonthEditText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length != tilMonthLength) {
+                    tilMonth.error = "Invalid month"
+                    tilMonth.isErrorEnabled = true
+                } else {
+                    tilMonth.error = null
+                    tilMonth.isErrorEnabled = false
+                }
+
+                if ( (s?.length == tilMonthLength) && (s.toString().toInt() > 12)) {
+                    tilMonth.error = "Invalid month"
+                    tilMonth.isErrorEnabled = true
+                } else if ( (s?.length == tilMonthLength) && (s.toString().toInt() <= 12)) {
+                    tilMonth.error = null
+                    tilMonth.isErrorEnabled = false
+                }
+                updateButtonState()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){}
+        })
+
+        btPay.setOnClickListener {
+            _holderName = tilHolderName.editText?.text.toString()
+            _cardNumber = tilCardNumber.editText?.text.toString().toLong()
+            _month = tilMonth.editText?.text.toString()
+            _year = tilYear.editText?.text.toString()
+            _cvv = tilCvv.editText?.text.toString().toInt()
+
+            val cardDetails = CardClient(
+                "email@gmail.com",
+                _holderName!!,
+                _cardNumber!!,
+                _year!!+"-"+_month!!+"-01",
+                _cvv!!,
+                " ",
+                _holderName!!)
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BaseURL.BASE_URL.toString())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val paymentService = retrofit.create(CardService::class.java)
+            val request = paymentService.postCardClient(
+                    SharedPreferences(view.context).getValue("id")!!.toInt(),
+                    cardDetails)
+
+            request.enqueue(object : Callback<CardClient> {
+                override fun onResponse(call: Call<CardClient>, response: Response<CardClient>) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(context, "Payment successful", Toast.LENGTH_SHORT).show()
+                        //updateContractStatus(view)
+                        Navigation.findNavController(view)
+                            .navigate(R.id.action_fragment_client_pay_to_fragment_client_pay_success)
+                    } else {
+                        Toast.makeText(context, "Payment failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onFailure(call: Call<CardClient>, t: Throwable) {
+                    Toast.makeText(context, "Payment failed", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        val btCancel = view.findViewById<Button>(R.id.bt_cancel)
+
+        btCancel.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_fragment_client_pay_to_fragment_client_pay_success)
+        }
+
+    }
+
+    private fun updateContractStatus(view: View){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseURL.BASE_URL.toString())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val contractService = retrofit.create(CardService::class.java)
+        val request = contractService.updateContractStatus(
+            SharedPreferences(view.context).getValue("idNotification")!!.toInt(), 2)
+
+        request.enqueue(object : Callback<CardClient> {
+            override fun onResponse(call: Call<CardClient>, response: Response<CardClient>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(context, "Contract status updated", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Contract status update failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<CardClient>, t: Throwable) {
+                Toast.makeText(context, "Contract status update failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
